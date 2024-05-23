e7db2664f4d3398cd62cb04c95cef77f7e85da91
tinh <tinhkle@gmail.com>
2024-05-23 15:22:26 -0700

    add utils

--- wellness-journal/app/lib/utils.ts
+++ wellness-journal/app/lib/utils.ts
@@ -1,4 +1,25 @@
 import moment from 'moment-timezone';
 
 const TIMEZONE = "America/Los Angeles";
+
+export const getDate = (): string => moment().format('dddd, MMMM D, YYYY');
+
 export const getHour = (): number => moment().tz(TIMEZONE).hour();
+
+export const WEEKDAY_ABBREVIATIONS = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+export const getCurrentWeekDays = () => {
+   // Get the start of the current week
+  const today = moment().startOf('week');
+  const days = [];
+  
+  for (let i = 0; i < 7; i++) {
+    const day = today.clone().add(i, 'days').date();
+    days.push([WEEKDAY_ABBREVIATIONS[i], day]);
+  }
+
+  return days;
+}
+
+export const getDayOfMonth = () => {
+  return moment().date();
+};
